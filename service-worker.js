b307dd8d14fefdf77818a924ca487a91de00b2ea
leonardomendes081-lang <leonardomendes081@gmail.com>
2026-02-21 18:15:24 -0300

    Add service worker for caching and fetch handling
    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = "meu-app-v2.2";
 
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
